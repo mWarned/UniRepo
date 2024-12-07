d01fbafe7b0b124125c321bbfaf8162cde9ef485
mWarned <mihai.jizdan2021@gmail.com>
2024-12-07 20:55:20 +0200

    PC Lab 7

--- PC1/Lab7/test.sh
+++ PC1/Lab7/test.sh
@@ -6,22 +6,27 @@ NC='\033[0m'
 
 # Function to input values into the program
 input_values() {
-    local testnr=$1
-    local filename=$2
+  local testnr=$1
+  local filename=$2
 
-    echo -e "${HL}Testcase $testnr${NC}"
-    echo "Input data:"
-    cat $filename
+  echo -e "${HL}Testcase $testnr${NC}"
+  echo "Input data:"
+  cat $filename
 
-    ./ex2_1 < $filename
+  while IFS= read -r line; do
+    echo "$line" | ./ex2_1
+  done < "$filename"
+
+  echo -e "${HL}End of file reached. You can now input manually.${NC}"
+  exec < /dev/tty
 }
 
 # First input set
 input_values 1 "test1.in"
 echo ""
 # Second input set
-input_values 2 "test2.in"
+# input_values 2 "test2.in"
 echo ""
 # Third input set
-input_values 3 "test3.in"
+# input_values 3 "test3.in"
 
